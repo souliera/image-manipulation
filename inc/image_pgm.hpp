5fd9f7a603f6890949959435d5fdd442984fecf0
Arnaud Soulier <soulier.arnaud34@gmail.com>
2024-02-28 18:11:24 +0100

    Image PGM: Add empty & size constructors

--- inc/image_pgm.hpp
+++ inc/image_pgm.hpp
@@ -1,8 +1,33 @@
 #ifndef DEF_IMAGE_PGM_HPP
 #define DEF_IMAGE_PGM_HPP
 
-class ImagePGM {
+/**
+ * @brief File for the image class in PGM format.
+ * @file image_pgm.hpp
+ * @author Arnaud Soulier
+ * @date 28 feb. 2024
+ * @version 0.1.0
+ */
 
+#include <cstdint> /* (u)intX_t */
+#include <vector> /* vector class */
+#include <string> /* string class */
+
+/**
+ * @class ImagePGM
+ * @brief Class for the PGM image format.
+ */
+class ImagePGM {
+	private:
+		std::uint16_t _width;
+		std::uint16_t _height;
+		std::vector<std::uint16_t> _pixels;
+		std::string _magicNumber;
+		std::uint16_t _maxValue;
+
+	public:
+		ImagePGM();
+		ImagePGM(const std::uint16_t &width, const std::uint16_t &height, const std::uint16_t &maxValue);
 };
 
 #endif
--- src/image_pgm.cpp
+++ src/image_pgm.cpp
@@ -1,1 +1,5 @@
 #include "image_pgm.hpp"
+
+ImagePGM::ImagePGM() : ImagePGM(0, 0, 255) {}
+
+ImagePGM::ImagePGM(const std::uint16_t &width, const std::uint16_t &height, const std::uint16_t &maxValue) : _width(width), _height(height), _pixels(width * height, 0), _magicNumber("P5"), _maxValue(maxValue) {}
